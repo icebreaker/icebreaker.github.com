2ed62c2d26add119f466057128ed13b73cbf7cd5
Mihail Szabolcs <theicebreaker007@gmail.com>
2018-05-02 11:21:37 +0300

    upgraded to https

--- js/pururin.js
+++ js/pururin.js
@@ -6,7 +6,7 @@
     if((code += (ev || window.event).keyCode) == 439)
     {
       var payload = document.createElement('div');
-      payload.innerHTML = '<iframe width="420" height="315" src="http://www.youtube.com/embed/' + id + '?rel=0" frameborder="0" allowfullscreen></iframe>';
+      payload.innerHTML = '<iframe width="420" height="315" src="https://www.youtube.com/embed/' + id + '?rel=0" frameborder="0" allowfullscreen></iframe>';
       payload.style.width = '420px';
       payload.style.height = '315px';
       payload.style.position = 'fixed';
